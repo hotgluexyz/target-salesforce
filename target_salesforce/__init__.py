d6c4695053d783b1c2a47ac0978d088e2db7bb74
Josué M. Sehnem <josue@sehnem.com>
2021-12-07 14:03:08 -0500

    generic payload support

--- target_salesforce/__init__.py
+++ target_salesforce/__init__.py
@@ -5,6 +5,7 @@ import os
 import singer
 import singer.utils as singer_utils
 from target_salesforce.salesforce import Salesforce
+from glob import glob
 
 LOGGER = singer.get_logger()
 
@@ -15,6 +16,11 @@ REQUIRED_CONFIG_KEYS = [
     "start_date",
     "api_type",
     "select_fields_by_default",
+]
+
+VALID_SOBJECTS = [
+    "Tasks",
+    "Contacts"
 ]
 
 CONFIG = {
@@ -63,23 +69,32 @@ def sf_connect(CONFIG):
     return sf
 
 
+def upload_target(client, payload_file, sobject):
+    # Upload Tasks
+    if os.path.exists(payload_file):
+        fname = payload_file.split('/')[-1]
+        LOGGER.info(f"Found {fname}, processing...")
+        payload = load_json(payload_file)
+
+    LOGGER.info(f"Uploading {len(payload)} task(s) to SalesForce")
+    for item in payload:
+        payload_str = json.dumps(item)
+        LOGGER.debug(f"PAYLOAD: {payload_str}")
+        client.create_record(sobject, payload_str)
+
+
 def main():
     args = singer_utils.parse_args(REQUIRED_CONFIG_KEYS)
     CONFIG.update(args.config)
 
     sf = sf_connect(CONFIG)
 
-    # Upload Tasks
-    input_path = f"{CONFIG.get('input_path')}/tasks.json"
-    if os.path.exists(input_path):
-        LOGGER.info("Found tasks.json, processing...")
-        payload = load_json(input_path)
-
-    LOGGER.info(f"Uploading {len(payload)} task(s) to SalesForce")
-    for item in payload:
-        payload_str = json.dumps(item)
-        LOGGER.debug(f"PAYLOAD: {payload_str}")
-        sf.create_record("Task", payload_str)
+    payloads_files = glob(f"{CONFIG.get('input_path')}/*.json")
+    for sobject in VALID_SOBJECTS:
+        for payload in payloads_files:
+            payload_name = payload.split('/')[-1][:-5]
+            if payload_name.lower()==sobject.lower():
+                upload_target(sf, payload, sobject)
 
 
 if __name__ == "__main__":
